ff8d1d9d6342c88ff79685f491a9399429c03f7d
qwerji <bentswanson@gmail.com>
2017-05-22 09:23:15 -0700

    mobile background fix

--- script.js
+++ script.js
@@ -110,6 +110,5 @@ window.onload = () => {
     }
 }
 
-document.body.style.background = `url(./images/time-backgrounds/${
-    Math.floor((((new Date().getHours() - 4 + 24) % 24) / 24) * 8)
-}.png) no-repeat center center fixed`
+document.body.style.backgroundImage = `url(./images/time-backgrounds/${
+    Math.floor((((new Date().getHours() - 4 + 24) % 24) / 24) * 8)}.png)`
