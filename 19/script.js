34a0d9ad507d739111d65ec1f3cd239e522848ea
qwerji <bentswanson@gmail.com>
2017-06-06 16:05:25 -0700

    updates sudoku

--- 19/script.js
+++ 19/script.js
@@ -17,17 +17,17 @@
 // with an "onSolved" callback.
 
 const testPuzzle = [
-    [5, 0, 0,   0, 7, 0,   0, 0, 8],
-    [0, 0, 3,   0, 5, 0,   7, 0, 4],
-    [0, 0, 0,   1, 0, 0,   0, 6, 0],
+    [0, 6, 1,   0, 0, 7,   0, 0, 3],
+    [0, 9, 2,   0, 0, 3,   0, 0, 0],
+    [0, 0, 0,   0, 0, 0,   0, 0, 0],
 
-    [0, 0, 0,   6, 0, 0,   0, 5, 1],
-    [0, 0, 0,   2, 0, 5,   0, 0, 0],
-    [3, 1, 0,   0, 0, 7,   0, 0, 0],
+    [0, 0, 8,   5, 3, 0,   0, 0, 0],
+    [0, 0, 0,   0, 0, 0,   5, 0, 4],
+    [5, 0, 0,   0, 0, 8,   0, 0, 0],
 
-    [0, 2, 0,   0, 0, 9,   0, 0, 0],
-    [9, 0, 7,   0, 4, 0,   8, 0, 0],
-    [6, 0, 0,   0, 8, 0,   0, 0, 3]
+    [0, 4, 0,   0, 0, 0,   0, 0, 1],
+    [0, 0, 0,   1, 6, 0,   8, 0, 0],
+    [6, 0, 0,   0, 0, 0,   0, 0, 0]
 ]
 
 const inputs = document.querySelectorAll('.box'),
@@ -102,7 +102,7 @@ function endAnimation(val) {
 }
 
 function animateGrid() {
-    display.style.animationName = 'flow'
+    display.style.animationName = 'spin'
     let delay = 0
     for (let i = 0; i < inputGrid.length; i++) {
         for (let j = 0; j < inputGrid[i].length; j++) {
